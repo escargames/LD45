68776db8fab5d5b77f977f8297a9196259589325
Niarkou <claire.plantet@gmail.com>
2019-04-28 18:26:03 +0200

    menu updated

--- mode_menu.lua
+++ mode_menu.lua
@@ -2,11 +2,16 @@ function mode.menu.start()
     main = true
     cursor_x = 36
     cursor_y = {50, 65, 80}
+    levels = {"blablabla"}
     pos = 1
 end
 
 function mode.menu.update()
-    pos += ((btnp(2) and (pos > 1) and -1) or (btnp(3) and (pos < #cursor_y) and 1 or 0))
+    if main then
+        pos += ((btnp(2) and (pos > 1) and -1) or (btnp(3) and (pos < #cursor_y) and 1 or 0))
+    else
+        pos += ((btnp(2) and (pos > 1) and -1) or (btnp(3) and (pos < #levels) and 1 or 0))
+    end
 
     if main then
         cursor_x = 36
@@ -14,10 +19,13 @@ function mode.menu.update()
         cursor_x = 61
     end
 
-    if pos == 1 and btnp(4) and main then
+    if pos == 1 and (btnp(4) or btnp(1)) and main then
         main = false
     elseif main == false and btnp(4) then
         state = "test"
+    elseif main == false and (btnp(5) or btnp(0)) then
+        pos = 1
+        main = true
     end
 end
 
@@ -44,7 +52,7 @@ end
 function draw_menu_play()
     if main and pos == 1 or (not main) then
         cosprint("levels", 70, 35, 6, 9)
-        cosprint("blablabla", 75, cursor_y[1], 6, pos == 1 and not main and 9 or 11)
+        cosprint(levels[1], 75, cursor_y[1], 6, pos == 1 and not main and 9 or 11)
     end
 end
 
