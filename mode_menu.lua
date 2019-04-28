06b3baa1b0561c2e0c4fffc8544a29d003d5c86d
Niarkou <claire.plantet@gmail.com>
2019-04-28 18:14:52 +0200

    menu and map for the menu

--- mode_menu.lua
+++ mode_menu.lua
@@ -1,23 +1,58 @@
 function mode.menu.start()
-    cursor_x = {50, 65}
+    main = true
+    cursor_x = 36
+    cursor_y = {50, 65, 80}
     pos = 1
 end
 
 function mode.menu.update()
-    pos += ((btnp(2) and (pos > 1) and -1) or (btnp(3) and (pos < #cursor_x) and 1 or 0))
+    pos += ((btnp(2) and (pos > 1) and -1) or (btnp(3) and (pos < #cursor_y) and 1 or 0))
+
+    if main then
+        cursor_x = 36
+    else
+        cursor_x = 61
+    end
+
+    if pos == 1 and btnp(4) and main then
+        main = false
+    elseif main == false and btnp(4) then
+        state = "test"
+    end
 end
 
 function mode.menu.draw()
-    cls()
-    csprint("ld44", 30, 9, 11)
-    csprint("play", cursor_x[1], 6, (pos == 1 and 9 or 11))
-    csprint("buttons", cursor_x[2], 6, (pos == 2 and 9 or 11))
+    cls(3)
     draw_menu_ui()
+    csprint("ld44", 15, 9, 11)
+    cosprint("play", 5, cursor_y[1], 6, (pos == 1 and main and 9 or 11))
+    cosprint("help", 5, cursor_y[2], 6, (pos == 2 and main and 9 or 11))
+    cosprint("scores", 5, cursor_y[3], 6, (pos == 3 and main and 9 or 11))
+    draw_menu_play()
+    draw_menu_help()
     draw_menu_debug()
 end
 
 function draw_menu_ui()
-    line(40, cursor_x[pos], 40, cursor_x[pos] + 4, 11)
+    palt(0, false)
+    palt(15, true)
+    map(0, 48, 0, 0, 16, 16)
+    palt()
+    spr(42, cursor_x, cursor_y[pos] - 2)
+end
+
+function draw_menu_play()
+    if main and pos == 1 or (not main) then
+        cosprint("levels", 70, 35, 6, 9)
+        cosprint("blablabla", 75, cursor_y[1], 6, pos == 1 and not main and 9 or 11)
+    end
+end
+
+function draw_menu_help()
+    if main and pos==2 then
+        cosprint("controls", 70, 35, 6, 9)
+        cosprint("truc to truc", 70, cursor_y[1], 6, 11)
+    end
 end
 
 function draw_menu_debug()
