d9414be25b4db717eca366d1e24b2fbf385f4600
Sam Hocevar <sam@hocevar.net>
2019-10-07 07:34:38 +0200

    Press D for debug.

--- debug.lua
+++ debug.lua
@@ -1,28 +1,36 @@
 
-local old_draw = _draw
-local cpu_hist = {}
+do
+    local old_draw = _draw
+    local cpu_hist = {}
+    local on=false
+    local c=0
 
-function _draw()
-    old_draw()
+    function _draw()
+        old_draw()
 
-    font_outline(1)
-    print(stat(7).." fps", 89, 26, 8)
-    if game then
-        print("x="..game.player.x, 2, 2, 11)
-        print("y="..game.player.y, 2, 10, 11)
-    end
-    local cpu = 100*stat(1)
-    local max_cpu = cpu
-    add(cpu_hist, cpu)
-    if #cpu_hist > 50 then
-        for i=1,50 do
-            cpu_hist[i] = cpu_hist[i+1]
-            max_cpu = max(max_cpu, cpu_hist[i])
+        if btnp(3,1) then on = not on end
+        if not on then return end
+
+        c=c%2+0.5
+        --font_outline(1)
+        pico8_print(stat(7).." fps", 99, 26, c)
+        if game then
+            pico8_print("x="..game.player.x, 2, 2, c)
+            pico8_print("y="..game.player.y, 2, 10, c)
         end
-        cpu_hist[51] = nil
+        local cpu = 100*stat(1)
+        local max_cpu = cpu
+        add(cpu_hist, cpu)
+        if #cpu_hist > 50 then
+            for i=1,50 do
+                cpu_hist[i] = cpu_hist[i+1]
+                max_cpu = max(max_cpu, cpu_hist[i])
+            end
+            cpu_hist[51] = nil
+        end
+        pico8_print("cpu="..ceil(cpu), 99, 12, c)
+        pico8_print("max="..ceil(max_cpu), 99, 19, c)
+        --font_outline()
     end
-    print("cpu="..ceil(cpu), 89, 12, 14)
-    print("max="..ceil(max_cpu), 89, 19, 8)
-    font_outline()
 end
 
