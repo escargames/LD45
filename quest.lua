a9c5b8a0017353eab0aa9095575b7d05af5c5a50
Sam Hocevar <sam@hocevar.net>
2019-10-08 00:30:46 +0200

    Fix too many tokens.

--- quest.lua
+++ quest.lua
@@ -15,56 +15,15 @@ function new_quest()
               text="You found a pair of\ngloves! You can now\npush boulders with 🅾️." },
         },
         boulders = {
-            { x=10,y=13 },
-            { x=11,y=13 },
-            { x=12,y=15 },
-            { x=13,y=13 },
-            { x=11,y=31 },
-            { x=14,y=30 },
-            { x=12,y=32 },
-            { x=13,y=30 },
-            { x=12,y=30 },
-            { x=11,y=33 },
-            { x=13,y=33 },
-            { x=15,y=32 },
-            { x=15,y=33 },
-            { x=12,y=34 },
-            { x=14,y=31 },
-            { x=6,y=32 },
-            { x=14,y=32 },
-            { x=1,y=37 },
-            { x=0,y=38 },
-            { x=2,y=38 },
-            { x=6,y=44 },
-            { x=7,y=43 },
-            { x=23,y=38 },
-            { x=23,y=37 },
-            { x=24,y=37 },
-            { x=20,y=40 },
-            { x=21,y=40 },
-            { x=22,y=40 },
-            { x=23,y=40 },
-            { x=24,y=40 },
-            { x=21,y=41 },
-            { x=23,y=41 },
-            { x=20,y=42 },
-            { x=23,y=42 },
-            { x=24,y=42 },
-            { x=23,y=43 },
-            { x=22,y=44 },
-            { x=13,y=39 },
-            { x=14,y=39 },
-            { x=15,y=39 },
-            { x=12,y=41 },
-            { x=14,y=40 },
-            { x=15,y=40 },
-            { x=17,y=40 },
-            { x=11,y=42 },
-            { x=12,y=43 },
-            { x=23,y=7 },
-            { x=57,y=23 },
-            { x=30,y=29 },
-            { x=48,y=25 },
+            10,13,  11,13,  12,15,  13,13,  11,31,  14,30,  12,32,  13,30,  12,30,  11,33,  13,33,  15,32,  15,33,
+            12,34,  14,31,  6,32,  14,32,  1,37,
+            0,38,  2,38,  6,44,  7,43,
+            23,38,  23,37,  24,37,  20,40,  21,40,  22,40,  23,40,  24,40,  21,41,  23,41,  20,42,  23,42,  24,42,  23,43,  22,44,
+            13,39,  14,39,  15,39,  12,41,  14,40,  15,40,  17,40,  11,42,  12,43,
+            23,7,
+            57,23,
+            30,29,
+            48,25,
         },
         keys = {
             { x=18, y=21 },
@@ -118,13 +77,13 @@ function new_quest()
 
             { x=15, y=25,  id=g_id_person, name="Frdy",
               text = { "Good morning!",
-                       "Did the storm do any damage\nto your lovely home?", 
+                       "Did the storm do any damage\nto your lovely home?",
                        "I just can't believe my\nbeloved plants are still\nintact!",
                        "I heard the storm destroyed\na house by the lake.","I am going there to help\nrebuilding it!",
                        "Can you water my plants\nwhile I am gone?", -- question followed by answers
                        { "Thank you my dear friend!\nMy plants mean so much\nto me.","Taking care of them\nand watching them grow is\nmy biggest joy in the world." }, -- ♥
                        { "Don't worry,\nI am sure you will find it\nvery easy and relaxing!\nYou will do great." },              -- ?
-                       { "Aww, I didn't know you\nloved gardening too!","We should get together\nsometimes when I come back,\nI would love to hear about\nyour plants." },     -- !         
+                       { "Aww, I didn't know you\nloved gardening too!","We should get together\nsometimes when I come back,\nI would love to hear about\nyour plants." },     -- !
                        function()
                            sfx(g_sfx_loot1)
                            sfx(g_sfx_loot2)
@@ -138,7 +97,7 @@ function new_quest()
 
             { x=25, y=22,  id=g_id_person, name="Clemon",
               text = { "Well be with you, gentleman!",
-                       "Let me narrate a riddle\nfor thee:", 
+                       "Let me narrate a riddle\nfor thee:",
                        "Is this a raccoon\nthat I see before me,\nthe muzzle toward\nmy hand?","Come, let me clutch thee.",
                        "I have thee not,\nand yet I see thee thrice.",
               },
@@ -151,7 +110,7 @@ function new_quest()
                        "If by any chance you find\nthem on your way,\nwould you be kind enough\nto send them back to me?","I have been worrying\nlike hell.",  -- question followed by answers
                        { "You are right, I should\ntake care of myself and\ntry not to worry too much.","Thank you,\nit helps to know\nthat I am not alone." }, -- ♥
                        { "Yes it was a\nvery strong wind!\nThey love storms so much,\nthey couldn't resist\ngoing out." },              -- ?
-                       { "They are ressourceful\nlittle girls!","I would not be surprised\nif they found their\nway home by themselves!" },     -- !         
+                       { "They are ressourceful\nlittle girls!","I would not be surprised\nif they found their\nway home by themselves!" },     -- !
               },
               text2 = { "Thank you for sending\nback my girls."}
             },
@@ -164,7 +123,7 @@ function new_quest()
                        { "You are so sweet.\nI could really use\nsomeone to talk to,\nthank you so much.","I promise I will not stay alone\nand come to you whenever I feel anxious." }, -- ♥
                        { "My friends and I\nhave so much ideas\non how to improve things!",
                        "We are already taking part\nin some peaceful actions\nto change things,\nyou should come by sometimes." },              -- ?
-                       { "That's inspiring!\nI agree, collective action\ncan feel very empowering." },     -- !         
+                       { "That's inspiring!\nI agree, collective action\ncan feel very empowering." },     -- !
               }
             },
 
@@ -175,7 +134,7 @@ function new_quest()
               "So, I have been reading.","Did you know that\nthe temperature of\nlightning is around\n20000°C?",  -- question followed by answers
                        { "No, you are amazing!","After all,\nit is you who gave me\nmy very first science book\na while ago." }, -- ♥
                        { "I learned it in\na book my grandma found.\nOh! That's the one!\nWhere did you find it?" },              -- ?
-                       { "Amazing, right?\nScience facts are\nso fascinating.","Did you know that\none day on Venus\nis longer than a year\non Earth?" },     -- !         
+                       { "Amazing, right?\nScience facts are\nso fascinating.","Did you know that\none day on Venus\nis longer than a year\non Earth?" },     -- !
               }
             },
 
@@ -209,9 +168,10 @@ function init_quest(q)
     foreach(q.triggers, function(o)
         add(game.specials, { x=o.x+.5, y=o.y+.5, id=g_id_trigger, data=o, noblock=true })
     end)
-    foreach(q.boulders, function(o)
+    for i=1,#q.boulders,2 do
+        local o = {x=q.boulders[i], y=q.boulders[i+1]}
         add(game.specials, { x=o.x+.5, y=o.y+.5, id=g_spr_boulder, data=o, xoff=-4, yoff=-6 })
-    end)
+    end
     foreach(q.signs, function(o)
         add(game.specials, { x=o.x+.5, y=o.y+.5, id=g_spr_sign, data=o, xoff=-4, yoff=-6 })
     end)
